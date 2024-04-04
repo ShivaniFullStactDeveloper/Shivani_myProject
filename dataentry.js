276e98eb8173ed6490f84376fe80ee6c19075941
ShivaniFullStactDeveloper <156500244+ShivaniFullStactDeveloper@users.noreply.github.com>
2024-04-04 23:56:48 +0530

    Add files via upload

---
+++ dataentry.js
@@ -0,0 +1,10 @@
+
+function formDisplay(){
+    document.getElementById("page-One").hidden=false;
+    document.getElementById("page-Two").hidden=true;
+}
+
+function listDislay(){
+    document.getElementById("page-One").hidden=true;
+    document.getElementById("page-Two").hidden=false;
+}
