f5880d9fa2b2cbf7d2df3870a1dea9b309696cc7
ShivaniFullStactDeveloper <156500244+ShivaniFullStactDeveloper@users.noreply.github.com>
2024-04-15 22:26:14 +0530

    Add files via upload

--- dataentry.js
+++ dataentry.js
@@ -1,64 +1,103 @@
-let currentCount = 1;
 // array
-let student =[ <tr>
-    <td id="sr-no">1</td>
-    <td id="roll-no">001</td>
-    <td id="name"></td>
-    <td id="number"></td>
-    <td id="date"></td>
-    <td id="mail-id"></td>
-    <td id="education"></td>
-    <td id="add"></td>
-</tr>];
+let student = [];
 
-// first page show
-function formDisplay(){
-    document.getElementById("page-One").hidden=false;
-    document.getElementById("page-Two").hidden=true;
+// form page 
+function formDisplay() {
+    console.log("formDisplay");
+    document.getElementById("page-One").hidden = false;
+    document.getElementById("page-Two").hidden = true;
 }
 
-// inputing data
-function inputData(){
+// Add data in form
+function addData() {
     // dictionary
-    currentCount++;
-
-    console.log("inputdata");
+    
     let information = {};
+    
     // inputs
     information["firstName"] = document.getElementById("first-name").value;
     information["lastName"] = document.getElementById("last-name").value;
-    information["contactNo"] = document.getElementById("contact").value;
-    information["birth"] = document.getElementById("dob").value;
+    information["contactNo"] = document.getElementById("contact-no").value;
+    information["birth"] = document.getElementById("birth-date").value;
     information["email"] = document.getElementById("email-id").value;
-    information["education"] = document.getElementById("edu-details").value;
-    information["address"] = document.getElementById("address").value;
-
-    // push 
-    student.push(information);
-    student.push('')
     
+    // if(education == graduate){
+    //     document.getElementById("edu-details").value = "Graduare";
+    // }else{
+    //     document.getElementById("edu-details").value = "PostGraduare";
+    // }
+    
+    information["education"] = document.getElementById("edu-details").value;
+    information["address"] = document.getElementById("address-l").value;
 
-    for(let i=0; i < student.length; i++){
-        let output = student[i];
-        
-        console.log(student,information);
+    student.push(information);
 
-        document.getElementById("name").innerHTML= output.firstName + output.lastName; 
-        document.getElementById("number").innerHTML= output.contactNo;  
-        
-    }
+    console.log(student,information);
+
+//clear form data
+    document.getElementById("first-name").value = "";
+    document.getElementById("last-name").value = "";
+    document.getElementById("contact-no").value = "";
+    document.getElementById("birth-date").value = "";
+    document.getElementById("email-id").value = "";
+    document.getElementById("edu-details").value = "";
+    document.getElementById("address-l").value = "";
 }
 
-// sec page show
-function listDisplay(){
+//list page 
+function listDisplay() {
     console.log("listDisplay");
-    document.getElementById("page-One").hidden=true;
-    document.getElementById("page-Two").hidden=false;
-
-   
+    document.getElementById("page-One").hidden = true;
+    document.getElementById("page-Two").hidden = false;
+    // calling function
+    showData();
 }
 
+// data show in list
+function showData(){
+    let tableOutput = "<table>";
 
-window.onload = function(){
+    tableOutput = ["<thead><tr><th>Student Name</th><th>Contact No</th><th>DOB</th><th>Email </th><th>Education Details</th><th>Address</th> </tr></thead>"]
+    
+    for (let i = 0; i < student.length; i++) {
+        let output = student[i];
+
+        let outputString =
+          '<tr><td class="name">' +
+          output.firstName +
+          " " +
+          output.lastName +
+          '</td><td class="mail-id">' +
+          output.contactNo +
+          '</td><td>' +
+          output.birth +
+          '</td><td>'+
+          output.email +
+          '</td><td>' +
+          output.education +
+          '</td><td>' +
+          output.address +
+          '</td></tr>';
+        tableOutput += outputString;
+        console.log("addData", output);
+      }
+
+      tableOutput += "</table>";
+
+      document.getElementById("show-info").innerHTML = tableOutput;
+    }
+
+// first page show
+window.onload = function () {
     formDisplay();
-   }
+}
+
+/* <th>Sr No</th><th>Roll No</th> */
+
+document.getElementById("first-name").value = "";
+document.getElementById("last-name").value = "";
+document.getElementById("contact-no").value = "";
+document.getElementById("birth-date").value = "";
+document.getElementById("email-id").value = "";
+document.getElementById("edu-details").value = "";
+document.getElementById("address-l").value = "";
